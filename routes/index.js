3bf214661244f986dc0075c9ceed7ba6304e1a88
lucero <lucero.gutierrez@unmsm.edu.pe>
2024-10-17 11:31:22 -0500

    Commit - Cambio de titulo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Invitado' });
+  res.render('index', { title: 'Rouge' });
 });
 
 module.exports = router;
